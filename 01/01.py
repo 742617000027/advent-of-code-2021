8da345e09be2a08a9527bef7563949a481641d8a
742617000027 <15521878+742617000027@users.noreply.github.com>
2021-12-01 20:51:30 +0100

    added timer

--- 01/01.py
+++ 01/01.py
@@ -1,25 +1,23 @@
-from time import process_time
-
 import numpy as np
 
 import utils
 
 if __name__ == '__main__':
+    timer = utils.Timer()
+
     # Part 1
     """
-    tic = process_time()
+    timer.start()
     data = utils.read_int_sequence()
     N = len([x for i, x in enumerate(data[1:]) if x > data[i]])
     print(N)
-    toc = process_time()
-    print(f'fininshed in {1000 * (toc - tic):.2f}ms') # 2.08ms
+    timer.stop()
     """
 
     # Part 2
-    tic = process_time()
+    timer.start()
     data = utils.read_int_sequence()
     conv = np.convolve(np.array(data), np.ones(3), mode='valid')
     N = len([x for i, x in enumerate(conv[1:]) if x > conv[i]])
     print(N)
-    toc = process_time()
-    print(f'fininshed in {1000 * (toc - tic):.2f}ms')  # 2.08ms
+    timer.stop()
