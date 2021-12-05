1f018cf485d86038ec2db6349ec543717e75a341
742617000027 <15521878+742617000027@users.noreply.github.com>
2021-12-06 00:12:59 +0100

    :D

--- 05/05_alt.py
+++ 05/05_alt.py
@@ -7,12 +7,13 @@ import utils
 def parse_lines(data, mode='HV'):
     assert mode in ['HV', 'all'], 'Mode must be one of `HV` or `all`.'
     diagram = defaultdict(lambda: defaultdict(lambda: 0))
-    for line in data:
-        X, Y = [list(range(z1, z2 + (1 if z2 >= z1 else -1), 1 if z2 >= z1 else -1))
-                for z1, z2 in zip(*[tuple(int(x) for x in elem.split(',')) for elem in line.split(' -> ')])]
-        for x, y in zip(cycle(X) if len(X) == 1 else X, cycle(Y) if len(Y) == 1 else Y):
-            if len(X) == 1 or len(Y) == 1 or mode == 'all': diagram[x][y] += 1
 
+    for x, y, hv in [point for elem in [
+        list(zip(cycle(X) if len(X) == 1 else X, cycle(Y) if len(Y) == 1 else Y, cycle([len(X) == 1 or len(Y) == 1])))
+        for X, Y in [[list(range(z1, z2 + (1 if z2 >= z1 else -1), 1 if z2 >= z1 else -1)) for z1, z2 in
+                      zip(*[tuple(int(x) for x in elem.split(',')) for elem in line.split(' -> ')])] for line in data]]
+                     for point in elem]:
+        if hv or mode == 'all': diagram[x][y] += 1
     return diagram
 
 
