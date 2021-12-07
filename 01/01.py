0991739a8c8df79abc9dfee16874337b1f9ee71a
742617000027 <15521878+742617000027@users.noreply.github.com>
2021-12-07 14:38:02 +0100

    refactor to account for IO function changes

--- 01/01.py
+++ 01/01.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     # Part 1
     """
     timer.start()
-    data = utils.read_int_sequence()
+    data = utils.read_int_lines()
     N = len([x for i, x in enumerate(data[1:]) if x > data[i]])
     print(N)
     timer.stop()
@@ -16,7 +16,7 @@ if __name__ == '__main__':
 
     # Part 2
     timer.start()
-    data = utils.read_int_sequence()
+    data = utils.read_int_lines()
     conv = np.convolve(np.array(data), np.ones(3), mode='valid')
     N = len([x for i, x in enumerate(conv[1:]) if x > conv[i]])
     print(N)
