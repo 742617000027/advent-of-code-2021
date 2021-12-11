dcb8e436e4c3b957cc87a4d33ca6545355e68705
742617000027 <15521878+742617000027@users.noreply.github.com>
2021-12-11 11:55:34 +0100

    cleanup

--- 11/11.py
+++ 11/11.py
@@ -15,8 +15,7 @@ def perform_step(data):
             if (h, w) not in already_flashed:
                 data[h - 1:h + 2, w - 1:w + 2] += kernel
                 already_flashed.add((h, w))
-                has_to_flash = set(zip(*np.where(data > 9)))
-        has_to_flash = has_to_flash.difference(already_flashed)
+        has_to_flash = set(zip(*np.where(data > 9))) - already_flashed
     data[data > 9] = 0
     return data[1:-1, 1:-1]
 
@@ -25,7 +24,7 @@ if __name__ == '__main__':
     timer = utils.Timer()
 
     # Part 1
-    """
+    # """
     timer.start()
     data = np.array([[int(elem) for elem in line] for line in utils.read_str_lines()])
     total = 0
@@ -33,11 +32,11 @@ if __name__ == '__main__':
         data = perform_step(data)
         total += np.sum(data == 0)
     print(total)
-    timer.stop()  # 72.86ms
-    """
+    timer.stop()  # 46.34ms
+    # """
 
     # Part 2
-    # """
+    """
     timer.start()
     data = np.array([[int(elem) for elem in line] for line in utils.read_str_lines()])
     step = 1
@@ -47,5 +46,5 @@ if __name__ == '__main__':
             break
         step += 1
     print(step)
-    timer.stop()  # 230.62ms
-    # """
+    timer.stop()  # 133.46ms
+    """
