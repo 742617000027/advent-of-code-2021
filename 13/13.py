f65df2b7a8dc90cd5d2685373286fa6c26dc91b4
742617000027 <15521878+742617000027@users.noreply.github.com>
2021-12-13 09:59:50 +0100

    13

---
+++ 13/13.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import utils
+
+
+def parse_data(data):
+    points, fold = [], []
+    W, H = 0, 0
+    for line in data:
+        if ',' in line:
+            w, h = [int(elem) for elem in line.split(',')]
+            points.append((h, w))
+            if w > W:
+                W = w
+            if h > H:
+                H = h
+        elif 'fold' in line:
+            z = int(line.split('=')[1])
+            folds.append((0 if 'y' in line else 1, z))
+    grid = np.zeros((H + 1, W + 1))
+    for point in points:
+        grid[point] = 1
+    return grid, folds
+
+
+def fold_grid(fold, grid):
+    axis, z = fold
+    return np.sign(grid[:z, :] + np.flipud(grid[z + 1:, :]) if axis == 0 \
+                       else grid[:, :z] + np.fliplr(grid[:, z + 1:]))
+
+
+if __name__ == '__main__':
+    timer = utils.Timer()
+
+    # Part 1
+    """
+    timer.start()
+    data = utils.read_str_lines()
+    grid, folds = parse_data(data)
+    grid = fold_grid(folds[0], grid)
+    print(np.sum(grid))
+    timer.stop()  # 32.68ms
+    """
+
+    # Part 2
+    # """
+    timer.start()
+    data = utils.read_str_lines()
+    grid, folds = parse_data(data)
+    for fold in folds:
+        grid = fold_grid(fold, grid)
+    plt.imshow(grid)
+    plt.show()
+    timer.stop()  # 244.18ms
+    # """
