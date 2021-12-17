df2b619643daa06fc9c3cc46ed07e60eeb001a14
742617000027 <15521878+742617000027@users.noreply.github.com>
2021-12-17 23:24:18 +0100

    cleanup

--- 16/16.py
+++ 16/16.py
@@ -4,13 +4,10 @@ import utils
 
 
 def hex2bin(data):
-    ret = ''
-    for hexa in data:
-        ret += bin(int(hexa, base=16)).replace("0b", "").zfill(4)
-    return ret
+    return ''.join([bin(int(elem, base=16)).replace("0b", "").zfill(4) for elem in data])
 
 
-def parse_packet(binary):
+def unpack(binary):
     version = int(binary[:3], base=2)
     type_id = int(binary[3:6], base=2)
     body = binary[6:]
@@ -38,42 +35,45 @@ def evaluate(packet):
             return (local[0] == local[1]) * 1
 
 
+def parse_literal(packet, body):
+    literal = ''
+    for i in range(0, len(body), 5):
+        literal += body[i + 1:i + 5]
+        if body[i] == '0':
+            packet['len'] = 6 + len(body[:i + 5])
+            packet['value'] = int(literal, base=2)
+            break
+
+
+def parse_operator(packet, body):
+    length_type, body = body[0], body[1:]
+    length = 15 if length_type == '0' else 11
+    limit, body = int(body[:length], base=2), body[length:]
+    packet['len'] = 6 + 1 + length
+    packet['contents'] = []
+    bits_parsed, packets_parsed = 0, 0
+    while (bits_parsed if length_type == '0' else packets_parsed) < limit:
+        subpacket = dict()
+        interpreter(body[bits_parsed:], subpacket)
+        packet['version']['aggregate'] += subpacket['version']['aggregate']
+        bits_parsed += subpacket['len']
+        packets_parsed += 1
+        packet['contents'].append(subpacket)
+    packet['len'] += bits_parsed
+    packet['value'] = evaluate(packet)
+
+
 def interpreter(binary, packet):
-    version, type_id, body = parse_packet(binary)
-    packet['version'] = version
+    version, type_id, body = unpack(binary)
+    packet['version'] = {
+        'self': version,
+        'aggregate': version
+    }
     packet['typeId'] = type_id
-
     if type_id == 4:
-        literal = ''
-        for i in range(0, len(body), 5):
-            literal += body[i + 1:i + 5]
-            if body[i] == '0':
-                packet['len'] = len(binary[:6 + i + 5])
-                packet['value'] = int(literal, base=2)
-                break
+        parse_literal(packet, body)
     else:
-        length_type, body = body[0], body[1:]
-        length = 15 if length_type == '0' else 11
-        limit, body = int(body[:length], base=2), body[length:]
-        packet['len'] = 6 + 1 + length
-        packet['contents'] = []
-        bits_parsed, packets_parsed = 0, 0
-        while (bits_parsed if length_type == '0' else packets_parsed) < limit:
-            subpacket = dict()
-            interpreter(body[bits_parsed:], subpacket)
-            bits_parsed += subpacket['len']
-            packets_parsed += 1
-            packet['contents'].append(subpacket)
-        packet['len'] += bits_parsed
-        packet['value'] = evaluate(packet)
-
-
-def total_version(packet):
-    global total
-    total += packet['version']
-    if 'contents' in packet:
-        for child in packet['contents']:
-            total_version(child)
+        parse_operator(packet, body)
 
 
 if __name__ == '__main__':
@@ -85,9 +85,7 @@ if __name__ == '__main__':
     data = hex2bin(utils.read_str_lines()[0])
     packet = dict()
     interpreter(data, packet)
-    total = 0
-    total_version(packet)
-    print(total)
+    print(packet['version']['aggregate'])
     timer.stop()  # 1.78ms
     """
 
